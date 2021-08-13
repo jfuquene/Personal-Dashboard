f380f59b13dc69f6787ce37880836535ea0bcd80
John Fuquene <jfuquene@email.wm.edu>
2021-08-12 23:30:47 -0400

    basic fetch

---
+++ index.js
@@ -0,0 +1,7 @@
+let url = 
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+    })
