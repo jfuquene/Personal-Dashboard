b654aedcf37f424699384953faeb36dc497ee519
John Fuquene <jfuquene@email.wm.edu>
2021-08-16 23:57:10 -0400

    adding catch

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let url =
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        //  
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.body.innerHTML += data.user.name
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
